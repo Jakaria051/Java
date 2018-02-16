325ef83ada225ea6502ae5f145a993340376e855
Jakaria Ahmed Chowdhury <32171981+Jakaria051@users.noreply.github.com>
2018-02-16 23:22:17 +0600

    OOP

--- PolyMorphism_basic.java
+++ PolyMorphism_basic.java
@@ -17,3 +17,52 @@ public class PolyMorphism_basic {
     }
     
 }
+
+
+///now create new class named it MyFriend
+
+//sub class
+public class MyFriend extends MyFather {
+    
+     public void test()  //override method
+    {
+        System.out.println("Hello World ....from My Friend class");
+    }
+    
+}
+
+
+//then create new class named it MyBrother
+
+
+//sub class
+public class MyBrother extends MyFather {
+    
+     public void test()   //override method
+    {
+        System.out.println("Hello World ....from My Brother class");
+    }
+    
+}
+
+
+///finally create new class named it MyFather
+
+//super class
+public class MyFather {
+    
+    public void test()
+    {
+        System.out.println("Hello World ....from My father class");
+    }
+    
+}
+
+
+
+
+///output
+
+Hello World ....from My father class
+Hello World ....from My Brother class
+Hello World ....from My Friend class
