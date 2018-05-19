2f19e2499f1bbc4aaa759fae36c87f077aeb617f
Jakaria Ahmed Chowdhury <32171981+Jakaria051@users.noreply.github.com>
2018-05-19 23:57:55 +0600

    Add files via upload

---
+++ Stack_Bracket_Balanace.java
@@ -0,0 +1,67 @@
+
+package stack_bracket_balanace;
+
+import java.util.Stack;
+
+/**
+ *
+ * @author jakaria
+ */
+public class Stack_Bracket_Balanace {
+    
+    public static boolean isBalanced(String str)
+    {
+        Stack <Character> stack = new Stack<>();
+        char[] ch = str.toCharArray();
+        
+        for(char c : ch)
+        {
+            if(stack.empty())
+            {
+                stack.push(c);
+            }
+            else
+            {
+                char peek = stack.peek();
+                if(peek == '(' && c == ')')
+                {
+                    stack.pop();
+                }
+                else if(peek == '{' && c == '}')
+                {
+                    stack.pop();
+                }
+                else if(peek == '[' && c == ']')
+                {
+                    stack.pop();
+                }
+                else
+                {
+                    stack.push(c);
+                }
+            }
+        }
+        
+        
+        return stack.empty();
+        
+    }
+
+ 
+    public static void main(String[] args) {
+        
+        
+        String[] brackets = {
+            "(()){}()","({})","({}(","){})"
+        };
+        
+        for(String str : brackets)
+        {
+            String isbalance = isBalanced(str) ? "balanced" : "Unbalanced";
+            
+            System.out.println(str +" is " +isbalance);
+        }
+    
+    }
+    
+}
